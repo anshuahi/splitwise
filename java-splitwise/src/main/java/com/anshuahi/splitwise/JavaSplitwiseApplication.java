2c752095fb534d0c0f05a2e1a42ff95c00580a1a
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2026-03-18 22:33:59 +0530

    java project init

---
+++ java-splitwise/src/main/java/com/anshuahi/splitwise/JavaSplitwiseApplication.java
@@ -0,0 +1,13 @@
+package com.anshuahi.splitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSplitwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSplitwiseApplication.class, args);
+	}
+
+}
---
+++ java-splitwise/src/test/java/com/anshuahi/splitwise/JavaSplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshuahi.splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
